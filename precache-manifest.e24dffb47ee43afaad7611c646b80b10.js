c4194c5d27e5144957c233f3c07f8d64a491c760
reno finsa <reno.finsa@gmail.com>
2019-10-12 13:00:51 +0700

    first commit

---
+++ precache-manifest.e24dffb47ee43afaad7611c646b80b10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf94ac6290154bb2f0a6c092075fc53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d0d3134bc5875df6ec0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "99abc49e311206f0dafc",
+    "url": "/static/js/2.e32075be.chunk.js"
+  },
+  {
+    "revision": "4d0d3134bc5875df6ec0",
+    "url": "/static/js/main.f0ca5586.chunk.js"
+  },
+  {
+    "revision": "db652f3b85ef61cca86a",
+    "url": "/static/js/runtime-main.2afc10bc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
